65c004c8305ba3f69453c160743d789f8b43b721
lsc0x <101402482+RUqwe@users.noreply.github.com>
2022-12-14 19:12:56 +0300

    Update SilerTTS.py

--- SilerTTS.py
+++ SilerTTS.py
@@ -13,8 +13,14 @@ class STTSMod(loader.Module):
     self.client = client
   async def sttscmd(self, message):
     """Используй .stts <текст(максимальная длина 500 символов)>"""
+    text = utils.get_args_raw(message)
+    chat_id = message.chat_id
+    chat_uname = message.client.get_entity(chat_id)
+    
+    await message.client.send_message('silero_voice_bot', f'{text}')
     @message.client.on(events.NewMessage(chats='silero_voice_bot'))
     async def handler_new_message(event):
-      await message.client.send_message('me', event.message)
+      await message.client.send_message(f'{chat_uname}', event.message)
+      break
     
 
