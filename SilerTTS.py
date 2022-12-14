5d58ea7ebbe669452231471b379172955b98e16c
lsc0x <101402482+RUqwe@users.noreply.github.com>
2022-12-14 18:07:37 +0300

    Update SilerTTS.py

--- SilerTTS.py
+++ SilerTTS.py
@@ -4,8 +4,8 @@ from random import choice, randint
 from telethon.tl.types import DocumentAttributeFilename
 from .. import loader, utils
 
-@loader.tdsclass
-STTSMod(loader.Module):
+@loader.tds
+class STTSMod(loader.Module):
   """Module for bot Silero TTS"""
   strings = { "name": "STTS"}
   async def client_ready(self, client, db):
